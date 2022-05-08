a22ae4a490c3c40d62dc57195a2e98f51a928242
Pommelz <s.gsaenger@gmx.de>
2022-05-08 16:19:47 +0200

    added controller movement

--- Assets/02_Scripts/TestInput.cs
+++ Assets/02_Scripts/TestInput.cs
@@ -5,6 +5,14 @@ using static UnityEngine.InputSystem.InputAction;
 
 public class TestInput : MonoBehaviour
 {
+    [SerializeField] float speed = 5;
+    Vector3 inputVector;
+
+    //public delegate void StartTouchEvent(Vector2 position, float time);
+    //public event StartTouchEvent OnStartTouch;
+    //public delegate void EndTouchEvent(Vector2 position, float time);
+    //public event EndTouchEvent OnEndTouch;
+
     void Start()
     {
         
@@ -12,11 +20,13 @@ public class TestInput : MonoBehaviour
 
     void Update()
     {
-        
+        //OnMovement();
     }
 
     public void OnMovement(CallbackContext ctx)
     {
+        inputVector = new Vector3(ctx.ReadValue<Vector2>().x, ctx.ReadValue<Vector2>().y, 0);
+        transform.position += inputVector * speed;
         Debug.Log(ctx.ReadValue<Vector2>());
     }
 }
